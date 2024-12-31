efde35ff1deaa1414cae9be3b167e61f2e8ce898
ubb <ub3322125556@gmail.com>
2024-12-31 09:49:08 +0000

    carta

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const fireworks = document.getElementById('fireworks');
 const happyNewYear = document.getElementById('happyNewYear');
 const audio = document.getElementById('audio');
 
-const mensaje = "                                                                 para ti que eres una persona muy importante en mi vida te quiero desear el mejor año que todo lo que te propongas se te cumpla que te la pases muy bien con tus seres queridos y quiero que sepas que en la vida hay muchos momentos hermosos pero sin duda el mejor de ellos fue cuando te conoci no boy a olividar ese momento sin duda dicen que los sueños se hacen realidad y yo creo que si porque simpre soñe con poder conocer a una niña linda y tierna como tu y la verdad el verte sonreir la vedad que no tine presio simpre tratare de sacarte una sonrisa porque es lo que me hace muy feliz la vedad si quiero algo bonito con tigo niña de ojos bonitos (la pregunto es tu quieres) ATT:uribb❤ ";
+const mensaje = "                                                                 para ti que eres una persona muy importante en mi vida te quiero desear el mejor año que todo lo que te propongas se te cumpla que te la pases muy bien con tus seres queridos y quiero que sepas que en la vida hay muchos momentos hermosos pero sin duda el mejor de ellos fue cuando te conoci no boy a olividar ese momento sin duda dicen que los sueños se hacen realidad y yo creo que si porque simpre soñe con poder conocer a una niña linda y tierna como tu y la verdad el verte sonreir la vedad que no tiene presio simpre tratare de sacarte una sonrisa porque es lo que me hace muy feliz la vedad si quiero algo bonito con tigo niña de ojos bonitos (la pregunta es tu quieres) ATT:uribb❤ ";
 
 openButton.addEventListener('click', () => {
     
